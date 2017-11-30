a9a794474129bb156aea1e4549faa8146d36f7ba
Siddhant Agarwal <agarwal.siddhant23@gmail.com>
2017-12-01 00:57:26 +0530

    Fab animation added

--- app/src/main/java/com/siddhantagarwal/cryptomon/PersonalFragment.kt
+++ app/src/main/java/com/siddhantagarwal/cryptomon/PersonalFragment.kt
@@ -2,9 +2,11 @@ package com.siddhantagarwal.cryptomon
 
 import android.app.Fragment
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 
 /**
  * Created by siddhant on 30/11/17.
@@ -13,6 +15,9 @@ import android.view.ViewGroup
 class PersonalFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val view = inflater!!.inflate(R.layout.layout_fragment_personal, container, false)
+        val addTransactionButton = view.findViewById<FloatingActionButton>(R.id.add_transaction_button)
+        addTransactionButton.animation = AnimationUtils.loadAnimation(activity, R.anim.fab_grow)
+        addTransactionButton.animation.start()
         return view
     }
 }
